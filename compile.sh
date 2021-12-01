233697af13dd525f9c2d70b622312827428c562e
Pieloaf <piercelowe1@gmail.com>
2021-12-01 11:52:05 +0000

    updated repo file structure

--- compile.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "./build" ]; then
-    mkdir build
-fi
-
-# compile reader and writer
-g++ -I ./headers/ reader.cpp ./source/*.cpp -o ./build/reader
-g++ -I ./headers/ writer.cpp ./source/*.cpp -o ./build/writer
