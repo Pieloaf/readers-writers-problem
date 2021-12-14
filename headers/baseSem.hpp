32c399071170fa4ae572db07228cf54cb6c70ae1
Pieloaf <piercelowe1@gmail.com>
2021-12-14 12:34:10 +0000

    comments everywhere

--- headers/baseSem.hpp
+++ headers/baseSem.hpp
@@ -6,18 +6,25 @@
 #include <sys/sem.h>
 #include <stdio.h>
 
+// class definition for base semaphore
 class baseSem
 {
-private:
-    struct sembuf Ssembuf, Wsembuf;
-    int semid;
-
+    // public member function definitions
 public:
+    // overloaded constructor
     baseSem(int key, int nsems);
-    ~baseSem();
+    ~baseSem(); // default destructor
 
+    // sem wait function definition
     void wait(int sem_num);
+    // sem signal function definition
     void signal(int sem_num);
+
+    // private member variable definitions
+private:
+    // signal and wait semaphore buffer
+    struct sembuf Ssembuf, Wsembuf;
+    int semid; // int to store semaphore id
 };
 
 #endif
--- source/baseSem.cpp
+++ source/baseSem.cpp
@@ -1,31 +1,48 @@
 #include "baseSem.hpp"
 
+// Overloaded constructor implementation
 baseSem::baseSem(int key, int nsems)
 {
+	// get semaphore set identifier of `nsems` number of semaphores
+	// with with read-write permissions for everyone
     semid = semget(key, nsems, 0666 | IPC_CREAT);
+	
+	// for each semaphore
     for (int i = 0; i < nsems; i++)
     {
+		// initialise to 1 (unblocked)
         semctl(semid, i, SETVAL, 1);
     }
-    Wsembuf.sem_op = -1;
-    Wsembuf.sem_flg = SEM_UNDO;
+	
+	// Initialise sem op buffers //
+	// Wait Semaphore
+    Wsembuf.sem_op = -1; // decrement sem value when called
+    Wsembuf.sem_flg = SEM_UNDO; // set SEM_UNDO flag
 
-    Ssembuf.sem_op = 1;
-    Ssembuf.sem_flg = SEM_UNDO;
+	// Signal Semaphore 
+    Ssembuf.sem_op = 1; // increment sem value when called
+    Ssembuf.sem_flg = SEM_UNDO; // set SEM_UNDO flag
 }
 
+// default destructor
 baseSem::~baseSem()
 {
 }
 
+// implementation of wait function
 void baseSem::wait(int sem_num)
 {
+	// set sem number to perform operation on
     Wsembuf.sem_num = sem_num;
+	// call sem wait operation on sem set of id == semid
     semop(semid, &Wsembuf, 1);
 }
 
+// implementation of signal function
 void baseSem::signal(int sem_num)
 {
+	// set sem number to perform operation on
     Ssembuf.sem_num = sem_num;
+	// call sem signal operation on sem set of id == semid
     semop(semid, &Ssembuf, 1);
 }
