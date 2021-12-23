d8435e67123b1c12e6c1a803654315b93bb76998
Pierce Lowe <piercelowe1@gmail.com>
2021-12-23 16:09:38 +0000

    create data txt in build folder

--- writer-pref/compile.sh
+++ writer-pref/compile.sh
@@ -5,3 +5,4 @@ fi
 # compile reader and writer
 g++ -I ../headers/ reader.cpp ../source/*.cpp -o ./build/reader
 g++ -I ../headers/ writer.cpp ../source/*.cpp -o ./build/writer
+touch ./build/data.txt
